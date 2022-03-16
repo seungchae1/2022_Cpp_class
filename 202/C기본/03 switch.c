9d0b4982cf5109a5a155730fd202be0689d72b4f
seungchae <s2125@e-mirim.hs.kr>
2022-03-16 17:46:18 +0900

    switch break X

---
+++ 202/C기본/03 switch.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int x = 1;
+	// break가 없어서 "12d"가 출력
+	switch (x)
+	{
+	case 0:
+		printf("0");
+	case 1:
+		printf("1");
+	case 2:
+		printf("2");
+	default:
+		printf("d");
+	}
+
+	return 0;
+}
