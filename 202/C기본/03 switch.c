172f9ecb299a848de0e0c06c7d9b8b7e01c989f3
seungchae <s2125@e-mirim.hs.kr>
2022-03-17 11:03:10 +0900

    case에 break 일부 추가

--- 202/C기본/03 switch.c
+++ 202/C기본/03 switch.c
@@ -3,17 +3,20 @@
 int main(void)
 {
 	int x = 1;
-	// break가 없어서 "12d"가 출력
+	// 2에 break가 있어서 "12"가 출력
 	switch (x)
 	{
 	case 0:
 		printf("0");
+		break;
 	case 1:
 		printf("1");
 	case 2:
 		printf("2");
+		break;
 	default:
 		printf("d");
+		break;
 	}
 
 	return 0;
