6645631862a0c726cfe31644ae1b09d574819963
seungchae <s2125@e-mirim.hs.kr>
2022-03-17 12:12:26 +0900

    std:: 안써도 됨

--- 202/C++기본/기본.cpp
+++ 202/C++기본/기본.cpp
@@ -1,11 +1,14 @@
 // cout, endl 객체를 가지고 있음
 #include <iostream>
 
+//std:: 안써도 됨
+using namespace std;
+
 int main(void)
 {
 	// printf를 cout 객체로
 	// endl객체로 개행
-	std::cout << "안녕 세상아" << std::endl; //<<는 출력을 의미하는 연산자 //end line = 줄바꿈
+	cout << "안녕 세상아" << endl; //<<는 출력을 의미하는 연산자 //end line = 줄바꿈
 
 	return 0;
 }
