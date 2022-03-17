1af71cc6f631188c98e00288869fdc11dfe4462e
seungchae <s2125@e-mirim.hs.kr>
2022-03-17 11:31:41 +0900

    파일분할 전

---
+++ 202/C기본/04 함수의 정의.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+//함수의 선언(함수에 대한 메모리 공간 할당)
+int sum(int a, int b);
+
+int main(void)
+{	
+	printf("%d", sum(1, 2));
+	return 0;
+}
+
+//함수의 정의
+int sum(int a, int b)
+{
+	return a + b;
+}
