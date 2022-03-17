2e1a39958038e1f0cc65d73b82c9939120b76bb9
seungchae <s2125@e-mirim.hs.kr>
2022-03-17 11:49:13 +0900

    함수의 선언은 헤더파일로 함수의 정의는 c파일로 분할

--- 202/C기본/04 함수의 정의.c
+++ 202/C기본/04 함수의 정의.c
@@ -1,16 +1,8 @@
 ﻿#include <stdio.h>
-
-//함수의 선언(함수에 대한 메모리 공간 할당)
-int sum(int a, int b);
+#include "04.h"
 
 int main(void)
 {	
 	printf("%d", sum(1, 2));
 	return 0;
-}
-
-//함수의 정의
-int sum(int a, int b)
-{
-	return a + b;
 }
