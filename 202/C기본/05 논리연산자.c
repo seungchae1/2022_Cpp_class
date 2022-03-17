6245e9bc6dc877e37062fe838107f6bfb6219343
seungchae <s2125@e-mirim.hs.kr>
2022-03-17 11:56:44 +0900

    논리연산자

---
+++ 202/C기본/05 논리연산자.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void)
+{	
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	// a는 1로 증가한 직후에 if문을 참으로 처리
+	// b와 c는 증가하지 X
+	if (++a || ++b && ++c)
+		printf("참");
+
+	printf(" %d %d %d \n", a, b, c);
+
+	return 0;
+}
