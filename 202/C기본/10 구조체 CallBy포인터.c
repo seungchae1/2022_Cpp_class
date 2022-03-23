9c5dfa9749d439bdf3261dc6312c84113f7829f3
seungchae <s2125@e-mirim.hs.kr>
2022-03-23 09:55:25 +0900

    Call By Pointer로 jum의 x,y값이 변합니다

--- 202/C기본/10 구조체 CallBy포인터.c
+++ 202/C기본/10 구조체 CallBy포인터.c
@@ -5,22 +5,22 @@ typedef struct {
 	float y;
 }Jum;
 
-void AddValueToJum(Jum j);
+void AddValueToJum(Jum * j);
 
 int main(void)
 {	
 	Jum jum;
 	jum.x = 5.0f;
 	jum.y = 10.0f;
-	//Call By Value로 jum의 x,y값이 바뀌지 않음
-	AddValueToJum(jum);
+	//Call By Pointer로 jum의 x,y값이 바뀜
+	AddValueToJum(&jum);
 	printf("%.1f, %.1f \n", jum.x, jum.y);
 
 	return 0;
 }
 
-void AddValueToJum(Jum j)
+void AddValueToJum(Jum * j)
 {
-	j.x += 10.0f;
-	j.y += 15.0f;
+	j->x += 10.0f; // (*j).x += 10.0f;
+	j->y += 15.0f;
 }
