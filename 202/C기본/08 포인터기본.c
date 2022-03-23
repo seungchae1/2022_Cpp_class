0d3b8d17b4a6d4d761da1a55b7dcd0f356aac4cd
seungchae <s2125@e-mirim.hs.kr>
2022-03-23 09:13:15 +0900

    pointer 기본2

---
+++ 202/C기본/08 포인터기본.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main(void)
+{	
+	int num1 = 10, num2 = 20, num3 = 30;
+
+	//ptr1▶num1, ptr2▶num2, ptr3▶num3
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* ptr3 = &num3;
+
+	ptr1 = ptr2;	//ptr1▶num2
+	ptr2 = ptr3;	//ptr2▶num3
+
+	*ptr3 *= 2;		//num3 *= 2 -> num3는 90
+	*ptr1 += *ptr3;	//num2 += 2 -> num2는 120
+	*ptr2 *= 2;		//num3 *= 2 -> num3는 180
+
+	printf("%d %d %d \n", num1, num2, num3);
+
+	return 0;
+}
