72ae55c450d9b58f4511b469af3b579217df1651
seungchae <s2125@e-mirim.hs.kr>
2022-04-06 09:05:16 +0900

    매크로 함수01 - 의도치 않은 결과

--- 202/C++기본/02-01 매크로함수.cpp
+++ 202/C++기본/02-01 매크로함수.cpp
@@ -3,12 +3,15 @@
 //std:: 안써도 됨
 using namespace std;
 
+// pre컴파일러가 처리
+// 자료형에 영향받지 않는다
 #define jegob(A)	A * A
 
 int main(void)
 {
-	cout << jegob(3) << endl;
-	cout << 3 * 3 << endl;
+	//연산자 우선순위로 인해 의도치 않은 결과가 나옴
+	cout << jegob(3+1) << endl;		//7
+	cout << 3+1 * 3+1 << endl;		//7
 
 	return 0;
 }
