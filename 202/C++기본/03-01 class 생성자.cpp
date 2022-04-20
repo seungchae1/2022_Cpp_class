10da50a70ee63ed70f839b038999ba002a87f5d4
seungchae <s2125@e-mirim.hs.kr>
2022-04-20 09:17:06 +0900

    생성자 호출

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -8,9 +8,14 @@ class Student
 	int nHaknun;
 	string sName;
 
+public:
 	//생성자 : 객체가 생성될 때, 호출되는 함수
-	//디폴트 생성자를 컴파일러가 만들어준다.
-	//Student(){}
+	Student() 
+	{
+		nHaknun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다." << endl;
+	}
 };
 
 int main() 
