dad6401a6db5f4dead65ee2d534415ffdebdf378
seungchae <s2125@e-mirim.hs.kr>
2022-04-20 09:48:59 +0900

    멤버함수를 클래스 바깥에 정의

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -7,21 +7,9 @@ class Student
 {
 	int nHaknun;
 	string sName;
-
 public:
-	//생성자 : 객체가 생성될 때, 호출되는 함수
-	Student() 
-	{
-		nHaknun = 1234;
-		sName = "이사랑";
-		cout << "학번이 등록되었습니다." << endl;
-	}
-
-	void show()
-	{
-		cout << "학번은 " << nHaknun << "입니다" << endl;
-		cout << "이름은 " << sName << "입니다" << endl;
-	}
+	Student();
+	void show();
 };
 
 int main() 
@@ -32,4 +20,18 @@ int main()
 	stu1.show();
 	stu2.show();
 	return 0;
+}
+
+
+Student::Student()
+{
+	nHaknun = 1234;
+	sName = "이사랑";
+	cout << "학번이 등록되었습니다." << endl;
+}
+
+void Student::show()
+{
+	cout << "학번은 " << nHaknun << "입니다" << endl;
+	cout << "이름은 " << sName << "입니다" << endl;
 }
