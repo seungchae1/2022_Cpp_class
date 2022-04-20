ab2a863414cefea3d81974aacd37cc633a0ae36a
seungchae <s2125@e-mirim.hs.kr>
2022-04-20 09:41:59 +0900

    멤버함수 추가

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -16,6 +16,12 @@ public:
 		sName = "이사랑";
 		cout << "학번이 등록되었습니다." << endl;
 	}
+
+	void show()
+	{
+		cout << "학번은 " << nHaknun << "입니다" << endl;
+		cout << "이름은 " << sName << "입니다" << endl;
+	}
 };
 
 int main() 
@@ -23,5 +29,7 @@ int main()
 	Student stu1;
 	Student stu2 = Student();
 
+	stu1.show();
+	stu2.show();
 	return 0;
 }
