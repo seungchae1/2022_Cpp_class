a41af253f845de96215c4584b3a7bb4308eb7328
seungchae <s2125@e-mirim.hs.kr>
2022-04-20 10:17:11 +0900

    생성자 오버로딩

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -9,19 +9,23 @@ class Student
 	string sName;
 public:
 	Student();
+	Student(int Hakbun, string Name);
 	void show();
 };
 
 int main() 
 {
-	Student stu1;
-	Student stu2 = Student();
-
+	Student stu1 = { 1201,"곽경희" };
 	stu1.show();
+	
+	Student stu2 = Student(1202,"권하은");
 	stu2.show();
+
+	Student stu3;
+	stu3.show();
+
 	return 0;
 }
-
 
 Student::Student()
 {
@@ -30,8 +34,15 @@ Student::Student()
 	cout << "학번이 등록되었습니다." << endl;
 }
 
+Student::Student(int Hakbun, string Name)
+{
+	nHaknun = Hakbun;
+	sName = Name;
+	cout << "학번이 등록되었습니다." << endl;
+}
+
 void Student::show()
 {
 	cout << "학번은 " << nHaknun << "입니다" << endl;
-	cout << "이름은 " << sName << "입니다" << endl;
+	cout << "이름은 " << sName << "입니다" << endl << endl;
 }
