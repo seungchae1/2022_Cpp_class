0b509d22d08d501477e5e9b76e85c66b1521d8d7
seungchae <s2125@e-mirim.hs.kr>
2022-05-18 09:46:46 +0900

    복사 생성자(얕은 복사)

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -10,6 +10,9 @@ class Student
 public:
 	//생성자 : 객체가 생성될 때 호출되는 함수
 	Student(int Hakbun, const char* Name);
+	//복사생성자
+	Student(const Student& rhs);
+
 	//소멸자 : 객체가 메모리에서 해제될 때 생성되는 함수
 	~Student();
 
@@ -18,8 +21,14 @@ public:
 
 int main() 
 {
+	//일반 생성자 호출
 	Student stu1 = Student(1111, "JWP");
+	
+	//복사생성자 호출
+	Student stu2 = stu1; //Student stu2 = Student(stu1);
+
 	stu1.show();
+	stu2.show();
 
 	return 0;
 }
@@ -31,6 +40,12 @@ Student::Student(int Hakbun, const char* Name)
 	int len = strlen(Name) + 1;	//동적할당할 공간의 크기를 구하고
 	sName = new char[len];		//그 크기만큼 배열로 동적할당
 	strcpy(sName, Name);		// 매개변수에 있는 문자열 복사
+}
+
+Student::Student(const Student& rhs) 
+	:nHaknun(rhs.nHaknun),sName(rhs.sName)
+{
+	cout << "복사생성자 호출" << endl;
 }
 
 Student:: ~Student()
