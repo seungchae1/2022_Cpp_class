7ada6fac8670bd607af854e30a59a0384079bbd7
seungchae <s2125@e-mirim.hs.kr>
2022-05-19 11:14:16 +0900

    Person 클래스(부모 클래스)

---
+++ 202/C++기본/상속실습.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Person
+{
+	string sName;
+	int nAge;
+public:
+	Person();
+	Person(string name, int age);
+	void hi();
+
+};
+
+Person::Person()
+{
+	sName = "익명";
+	nAge = 0;
+}
+
+Person::Person(string name, int age)
+	:sName(name), nAge(age) {}
+
+void Person::hi()
+{
+	cout << "이름 : " << sName << endl;
+	cout << "나이 : " << nAge << endl;
+}
+
+int main()
+{
+	Person* person = new Person("한승원",100);
+	person->hi();
+	return 0;
+}
