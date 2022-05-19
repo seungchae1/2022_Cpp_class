97e8c1be2c97d235d40ad18c3c8239b9dadf230c
seungchae <s2125@e-mirim.hs.kr>
2022-05-19 13:22:29 +0900

    상속연습

---
+++ 202/C++기본/상속실습3.cpp
@@ -0,0 +1,18 @@
+class Phone {
+	void call();
+	void receive();
+	void sendMessage();
+};
+
+class Music {
+	void soundOn();
+	void soundOff();
+};
+
+class SmartPhone : Phone{
+
+};
+
+class SmartPhone2 :Music {
+
+};
