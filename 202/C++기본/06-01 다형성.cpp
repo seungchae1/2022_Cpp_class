06cddeea7ca19711aac0d172060cd540def0907a
seungchae <s2125@e-mirim.hs.kr>
2022-08-31 09:41:58 +0900

    가상 함수를 통한 동적바인딩 (다형성 완성)

--- 202/C++기본/06-01 다형성.cpp
+++ 202/C++기본/06-01 다형성.cpp
@@ -6,7 +6,8 @@ using namespace std;
 class Animal {
 public :
 	void eat() { cout << "µ¿¹°¸Ô¾î" << endl; }
-	void roar() { cout << "µ¿¹°Â¢¾î" << endl; }
+	//roarÇÔ¼ö¸¦ °¡»óÇÔ¼öÅ×ÀÌºí¿¡ µî·Ï(µ¿Àû ¹ÙÀÎµù)
+	virtual void roar() { cout << "µ¿¹°Â¢¾î" << endl; }
 	void walk() { cout << "µ¿¹°°É¾î" << endl; }
 	
 private:
@@ -17,12 +18,12 @@ private:
 
 class Tiger : public Animal{
 public :
-	void roar() { cout << "¾îÈï" << endl; }
+	void roar(void) override { cout << "¾îÈï" << endl; }
 };
 
 class Dog : public Animal {
 public:
-	void roar() { cout << "¸Û¸Û" << endl; }
+	void roar(void) override { cout << "¸Û¸Û" << endl; }
 };
 
 int main()
