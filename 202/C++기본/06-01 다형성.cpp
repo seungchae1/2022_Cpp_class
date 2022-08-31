2cdc5cb51451f5d45d1b7ff98ad12ad185877308
seungchae <s2125@e-mirim.hs.kr>
2022-08-31 09:56:06 +0900

    virtual 소멸자를 사용하지 않으면 메모리 누수 발생

--- 202/C++기본/06-01 다형성.cpp
+++ 202/C++기본/06-01 다형성.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 class Animal {
 public :
+	Animal() { cout << "Animal 생성" << endl; }
+	~Animal() { cout << "Animal 생성" << endl; }
+
 	void eat() { cout << "동물먹어" << endl; }
 	//roar함수를 가상함수테이블에 등록(동적 바인딩)
 	virtual void roar() { cout << "동물짖어" << endl; }
@@ -18,28 +21,26 @@ private:
 
 class Tiger : public Animal{
 public :
+	Tiger() { cout << "Tiger 생성" << endl; }
+	~Tiger() { cout << "Tiger 생성" << endl; }
+
+
 	void roar(void) override { cout << "어흥" << endl; }
 };
 
 class Dog : public Animal {
 public:
+	Dog() { cout << "Dog 생성" << endl; }
+	~Dog() { cout << "Dog 생성" << endl; }
+
+
 	void roar(void) override { cout << "멍멍" << endl; }
 };
 
 int main()
 {
-	Animal * animal = new Animal();
-	animal->roar();	// 동물짖어
+	Animal* animal = new Tiger();
 	delete animal;
-
-	animal = new Tiger();
-	animal->roar();// 동물짖어
-	delete animal;
-
-	animal= new Dog();
-	animal->roar();// 동물짖어
-	delete animal;
-
 
 	return 0;
 }
