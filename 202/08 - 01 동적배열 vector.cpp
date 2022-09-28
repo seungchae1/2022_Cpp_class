f9bac95ae33bd84488b63a1d02ba3271e8ad2d62
seungchae <s2125@e-mirim.hs.kr>
2022-09-28 10:03:30 +0900

    vector 원소 size() 사용해 출력

--- 202/08 - 01 동적배열 vector.cpp
+++ 202/08 - 01 동적배열 vector.cpp
@@ -6,27 +6,34 @@ using namespace std;
 
 int main()
 {
-	//크기가 10개로 고정된 정적배열
-	string s1[10];
-	string* s2 = new string[10];
-
 	//크기 확장이 가능한 동적배열
 	vector<string> s3;
 	s3.reserve(10); //크기를 10개로 확장
 
 	for (int i = 0; i < 10; i++)
 	{
-		s1[i] = "ab";
-		s2[i] = "ab";
 		s3.push_back("ab");
 	}
 
-	//idx범위 초과(공간 부족)
-	// s1[10] = "ab";
-	// s2[10] = "ab";
-
 	//기존공간에 크기를 확장하여 삽입
 	s3.push_back("ab");
+
+	//vector의 원소들 출력
+	for (int i = 0; i < s3.size(); i++)
+	{
+		cout << s3[i] << " " ;
+	}
+	cout << endl;
+
+	//vector의 데이터 수정
+	s3[1] = "cd";
+	s3.at(2) = "ef";
+
+	for (int i = 0; i < s3.size(); i++)
+	{
+		cout << s3[i] << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
