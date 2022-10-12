9b184c23122667012ed010601d314f03a6b71328
seungchae <s2125@e-mirim.hs.kr>
2022-10-12 09:45:04 +0900

    map 가독성있는 형태로 수정

--- 202/C++기본/08-02 map.cpp
+++ 202/C++기본/08-02 map.cpp
@@ -16,10 +16,10 @@ int main()
 {
 	//map<key 자료형, value자료형>
 	map<string, string> m;
-	m.insert(pair<string, string>("김신", "공유"));
-	m.insert(pair<string, string>("지은탁", "김고은"));
-	m.insert(pair<string, string>("저승사자", "이동욱"));
-	m.insert(pair<string, string>("써니", "유인나"));
+	m["김신"] = "공유";
+	m["지은탁"] = "김고은";
+	m["저승사자"] = "이동욱";
+	m["써니"] = "유인나";
 
 	//map은 데이터를 삽입할 때마다 key를 기준으로 자동정렬된다.
 
@@ -30,8 +30,7 @@ int main()
 		cout << iter->first << "역 " << iter->second << "님"<< endl;
 	}
 
-	map<string, string>::iterator actor = m.find("김신");
-	cout << "주인공은 " << actor->second << endl;
+	cout << "주인공은 " << m["김신"] << endl;
 
 	return 0;
 }
