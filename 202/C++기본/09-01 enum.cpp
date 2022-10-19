516d1b47a2e97290e844f9a88738ea6c8d25e65c
seungchae <s2125@e-mirim.hs.kr>
2022-10-19 09:12:32 +0900

    enum 적용 -> 가독성up

--- 202/C++기본/09-01 enum.cpp
+++ 202/C++기본/09-01 enum.cpp
@@ -3,22 +3,28 @@
 
 using namespace std;
 
+enum Food {
+	KOREAN,		// 한식(0)
+	JAPANESE,	// 일식(1)
+	CHINESE 	// 중식(2)
+};
+
 int main()
 {
 	string food_spot[3];
 
-	food_spot[0] = "삼백집";
-	food_spot[1] = "야키토리묵";
-	food_spot[2] = "태화장";
+	food_spot[Food::KOREAN] = "삼백집";
+	food_spot[Food::JAPANESE] = "야키토리묵";
+	food_spot[Food::CHINESE] = "태화장";
 
 	cout << "메뉴선택" << endl;
 	int menu = 1;
 
 	switch (menu)
 	{
-		case 0: cout << "한식 : "; break;
-		case 1: cout << "일식 : "; break;
-		case 2: cout << "중식 : "; break;
+		case Food::KOREAN : cout << "한식 : "; break;
+		case Food::JAPANESE : cout << "일식 : "; break;
+		case Food::CHINESE: cout << "중식 : "; break;
 		default: break;
 	}
 	cout << food_spot[menu] << endl;
