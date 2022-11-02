4aa2e0d2df4fb49de56473e47d280f13106c5826
seungchae <s2125@e-mirim.hs.kr>
2022-11-02 09:02:51 +0900

    범위기반 for문

--- 202/C++기본/과제.cpp
+++ 202/C++기본/과제.cpp
@@ -11,6 +11,7 @@ enum Department {
 
 int main()
 {
+	//1 enum 예제
 	int student[Department::count];
 	student[Department::software] = 28;
 	student[Department::websolution] = 26;
@@ -20,4 +21,9 @@ int main()
 	cout << "소프트웨어 : " << student[Department::software] << endl;
 	cout << "웹솔루션 : " << student[Department::websolution] << endl;
 	cout << "디자인 : " << student[Department::design] << endl;
+
+	//2 범위기반 for문
+	int arr[] = { 10, 20, 30, 40 };
+	for (int a : arr)
+		cout << a << endl;
 }
